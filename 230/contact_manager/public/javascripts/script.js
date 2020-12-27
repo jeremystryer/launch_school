f073b454c876bf1bfd9a3b1a52baba6bec93a2cf
Jeremy Stryer <jm.stryer@gmail.com>
2020-12-27 13:45:51 -0600

    Complete tagging feature

--- 230/contact_manager/public/javascripts/script.js
+++ 230/contact_manager/public/javascripts/script.js
@@ -5,7 +5,6 @@ class API {
       method: "GET",
       dataType: "JSON",
     }).done(json => {
-      console.log(json);
       callback(json);
     });
   }
@@ -47,7 +46,7 @@ class API {
       data: data,
       contentType: "application/json",
     }).done((json) => {
-      callback();
+      callback(json);
     });
   }
 }
@@ -59,120 +58,85 @@ class App {
   }
 
   init() {
-    this.api.getAllContacts(this.showAllContacts.bind(this));
-    let modal = document.querySelector(".modal");
-    modal.style.display = "none";
+    this.createTemplates();
+    this.bindEventHandlers();
+    this.showAllContacts();
   }
 
-  showAllContacts(contacts) {
-    let contactListSection = document.getElementById('contact-list');
-    let contactInfoTemplate = document.getElementById('contact-info-template').innerHTML;
-    let templateScript = Handlebars.compile(contactInfoTemplate);
-    contactListSection.innerHTML = templateScript(contacts);
-    this.addEventListeners();
+  createTemplates() {
+    this.templates = {};
+
+    document.querySelectorAll("script[type='text/x-handlebars']").forEach(tmpl => {
+      this.templates[tmpl["id"]] = Handlebars.compile(tmpl["innerHTML"]);
+    });
   }
 
-  addEventListeners() {
-    let editBtns = document.querySelectorAll('.edit');
+  bindEventHandlers() {
+    let editBtn = document.querySelector('.edit');
     let deleteBtns = document.querySelectorAll('.delete');
-    let addContactBtn = document.getElementById('add-contact-button');
+    let cancelBtn = document.querySelectorAll(".cancel");
     let searchField = document.getElementById('search-field');
-    let searchedName = '';
+    let addContactBtn = document.getElementById('add-contact-button');
 
-    searchField.addEventListener('keyup', event => {
-      let searchedName = searchField.value;
-      let contactNames = document.getElementsByClassName('full-name');
+    searchField.addEventListener('keyup', this.search);
+    addContactBtn.addEventListener('click', this.displayNewContactForm);
+    document.addEventListener('click', this.displayEditContactForm.bind(this));
+    document.addEventListener('click', this.cancel);
+    document.addEventListener('click', this.submit.bind(this));
+    document.addEventListener('click', this.deleteContact.bind(this));
+    document.addEventListener('click', this.filterByTag.bind(this));
+  }
 
-      let matches = ([...contactNames].filter(name => {
-        let regex = new RegExp('^' + searchedName, "i");
-        return name.innerText.match(regex);
-      }));
+  filterByTag() {
+    if (event.target.classList.contains('tag')) {
+      event.preventDefault();
+      let tags = document.getElementsByClassName('tag');
 
-      [...contactNames].forEach(name => {
-        if (!matches.includes(name)) {
-          name.parentElement.style.display = 'none';
+      [...tags].filter(tag => {
+        let ul = tag.parentElement.parentElement;
+        if (this.containsMatchingTag(ul, event.target.innerText)) {
+          tag.parentElement.parentElement.parentElement.style.display = 'inline-block';
         } else {
-          name.parentElement.style.display = 'inline-block';
+          tag.parentElement.parentElement.parentElement.style.display = 'none';
         }
-      });
-    });
+      })
+    }
+  }
 
-    addContactBtn.addEventListener('click', event=> {
-      let newContactForm = document.getElementById('new-contact-form').innerHTML;
-      let modal = document.querySelector(".modal");
-      let modalContent = document.querySelector(".modal-content");
+  containsMatchingTag(node, tagContent) {
+    return [...node.children].some(listItem => listItem.innerText === tagContent);
+  }
 
-      modalContent.innerHTML = newContactForm;
-      modal.style.display = "block";
+  search() {
+    let searchField = document.getElementById('search-field');
+    let searchedName = searchField.value;
+    let contactNames = document.getElementsByClassName('full-name');
 
-      let submitBtn = document.querySelector(".submit");
-      let cancelBtn = document.querySelector(".cancel");
+    let matches = ([...contactNames].filter(name => {
+      let regex = new RegExp('^' + searchedName, "i");
+      return name.innerText.match(regex);
+    }));
 
-      cancelBtn.addEventListener('click', event => {
-        modal.style.display = "none";
-      });
-
-      submitBtn.addEventListener('click', event => {
-        event.preventDefault();
-        let form = document.querySelector('form');
-        let formData = new FormData(form);
-        let object = {};
-
-        formData.forEach(function(value, key) {
-          if (value) {
-            object[key] = value;
-          }
-        });
-
-        let json = JSON.stringify(object);
-
-        this.api.createNewContact(json, this.init.bind(this));
-      });
-    });
-
-    document.addEventListener('click', event => {
-      event.preventDefault();
-      if ([...editBtns].includes(event.target)) {
-        let parentContactDiv = event.target.parentElement;
-        let contactId = parentContactDiv.getAttribute('data-contact-id');
-        this.api.getSelectedContact(this.showSelectedContact.bind(this), contactId);
-      }
-    });
-
-    document.addEventListener('click', event => {
-      event.preventDefault();
-      if ([...deleteBtns].includes(event.target)) {
-        let parentContactDiv = event.target.parentElement;
-        let contactId = parentContactDiv.getAttribute('data-contact-id');
-        let deleteConfirmation = window.confirm("Do you want to delete the contact?");
-
-        if (deleteConfirmation) {
-          this.api.deleteSelectedContact(this.init.bind(this), contactId);
-        }
+    [...contactNames].forEach(name => {
+      if (!matches.includes(name)) {
+        name.parentElement.style.display = 'none';
+      } else {
+        name.parentElement.style.display = 'inline-block';
       }
     });
   }
 
-  showSelectedContact(contact) {
+  displayNewContactForm() {
+    let newContactForm = document.getElementById('new-contact-form').innerHTML;
     let modal = document.querySelector(".modal");
     let modalContent = document.querySelector(".modal-content");
-    let editContactSection = document.getElementById("edit-contact");
-    let editContactInfoTemplate = document.getElementById('edit-contact-info-template').innerHTML;
-    let templateScript = Handlebars.compile(editContactInfoTemplate);
+    this.currentId = null;
+    modalContent.innerHTML = newContactForm;
+    modal.style.display = 'block';
+  }
 
-    this.currentId = contact.id;
-
-    modalContent.innerHTML = templateScript(contact);
-    modal.style.display = "block";
-
-    let submitBtn = document.querySelector(".submit");
-    let cancelBtn = document.querySelector(".cancel");
-
-    cancelBtn.addEventListener('click', event => {
-      modal.style.display = "none";
-    });
-
-    submitBtn.addEventListener('click', event => {
+  submit() {
+    if (event.target.classList.contains('submit')) {
       event.preventDefault();
       let form = document.querySelector('form');
       let formData = new FormData(form);
@@ -186,8 +150,86 @@ class App {
 
       let json = JSON.stringify(object);
 
-      this.api.updateSelectedContact(this.currentId, json, this.init.bind(this));
+      if (this.currentId) {
+        this.api.updateSelectedContact(this.currentId, json, this.init.bind(this));
+      } else {
+        this.api.createNewContact(json, this.init.bind(this));
+      }
+    }
+  }
+
+  dataWithParsedTags(contactsData) {
+    contactsData.forEach(data => {
+      let tagsArr = [];
+      let tags = data.tags.split(',');
+
+      tags.forEach(tag => {
+        tagsArr.push(tag.trim());
+      });
+
+      data.tags = tagsArr;
     });
+
+    return contactsData;
+  }
+
+  showAllContacts() {
+    let modal = document.querySelector(".modal");
+    let contactListSection = document.getElementById('contact-list');
+    let populateTemplate = (data) => {
+      let newData = this.dataWithParsedTags(data);
+      contactListSection.innerHTML = this.templates['contacts-template']({contacts: newData});
+    }
+
+    modal.style.display = "none";
+    this.api.getAllContacts(populateTemplate);
+  }
+
+  cancel() {
+    if (event.target.classList.contains('cancel')) {
+      let modal = document.querySelector(".modal");
+      modal.style.display = "none";
+    }
+  }
+
+  deleteContact() {
+    event.preventDefault();
+
+    if (event.target.classList.contains('delete')) {
+      let parentContactDiv = event.target.parentElement;
+      let contactId = parentContactDiv.getAttribute('data-contact-id');
+      let deleteConfirmation = window.confirm("Do you want to delete the contact?");
+
+      if (deleteConfirmation) {
+        this.api.deleteSelectedContact(this.showAllContacts.bind(this), contactId);
+      }
+    }
+  }
+
+  displayEditContactForm() {
+    event.preventDefault();
+    if (event.target.classList.contains('edit')) {
+      let parentContactDiv = event.target.parentElement;
+      let contactId = parentContactDiv.getAttribute('data-contact-id');
+      this.currentId = contactId;
+      this.showSelectedContact(contactId);
+    }
+  }
+
+  showSelectedContact(contact) {
+    let modal = document.querySelector(".modal");
+    let modalContent = document.querySelector(".modal-content");
+
+    function fillInEditContactForm(contact) {
+      let editContactSection = document.getElementById("edit-contact");
+      let editContactInfoTemplate = document.getElementById('edit-contact-info-template').innerHTML;
+      let templateScript = Handlebars.compile(editContactInfoTemplate);
+      modalContent.innerHTML = templateScript(contact);
+      modal.style.display = "block";
+    }
+
+    this.currentId = contact;
+    this.api.getSelectedContact(fillInEditContactForm, this.currentId);
   }
 }
 
